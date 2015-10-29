9fa568f1f53976ace530a9d65e05ed4e44423273
buaaksy <buaaksy@vip.qq.com>
2015-10-29 09:48:47 +0800

    递归，大于10的时候会溢出存在问题，比如20！

---
+++ Fact/Fact/Fact.cpp
@@ -0,0 +1,29 @@
+// Fact.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+long int fact(long int n);
+int main()
+	{
+	int t = 0;
+	
+	printf_s("请输入进行阶乘运算的整数：\r\n");
+	scanf_s("%d",&t);
+	if (t <= 1) {
+		printf_s("%d ！ = 1\r\n",t);
+	}
+	else {
+		long int d = fact(t);
+		printf_s("%d！ = %d\r\n",t,d);
+	}
+	getchar();
+	getchar();
+    return 0;
+}
+long int fact(long int n) {
+	if (n <= 1)
+		return 1;
+	else
+		return n*fact(n - 1);
+}
